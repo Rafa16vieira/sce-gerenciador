7fe3336a1881f3ba073cb7e7bca7a45db7b242aa
CarlosWGama <carloswgama@gmail.com>
2024-05-07 10:26:25 -0300

    Ajust das env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        firebase_apiKey: process.env.firebase_apiKey,
+        firebase_apiKey: process.env.firebase_authDomain,
+        firebase_apiKey: process.env.firebase_databaseURL,
+        firebase_apiKey: process.env.firebase_projectId,
+        firebase_apiKey: process.env.firebase_storageBucket,
+        firebase_apiKey: process.env.firebase_messagingSenderId,
+        firebase_apiKey: process.env.firebase_appId,
+        firebase_apiKey: process.env.firebase_measurementId,
+    }
+};
 
 export default nextConfig;
